d4ea521909bc79c344670bc81595753e3049d289
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-23 14:00:00 +0100

    remove `typeCode` argument from `createEngineer`

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const leadEngineer = createEngineer(leadEngineerDoc.leadEngineer, 'E');
 console.log(`Candidate: ${candidate.name}, ${candidate.type}`);
 console.log(`Lead engineer: ${leadEngineer.name}, ${leadEngineer.type}`);
 
-export function createEngineer(name, typeCode) {
+export function createEngineer(name) {
   return new Employee(name, 'E');
 }
--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ import { createEngineer } from './index.js';
 
 describe('createEngineer', () => {
   it('should create an employee, given a name and a type code', () => {
-    const employee = createEngineer('John Doe', 'E');
+    const employee = createEngineer('John Doe');
     expect(employee.name).toBe('John Doe');
     expect(employee.type).toBe('Engineer');
   });
